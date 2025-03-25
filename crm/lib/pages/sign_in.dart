efeeebf966a15e22d41a91454cca0a173d085329
KennOtieno <otiken001@gmail.com>
2025-03-25 07:42:47 -0700

    Added components folder

--- crm/lib/pages/sign_in.dart
+++ crm/lib/pages/sign_in.dart
@@ -4,38 +4,66 @@ class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
 
   @override
-  Widget build(BuildContext: context) {
+  Widget build(BuildContext = context) {
     return Scaffold(
       backgroundColor: Colors.grey(300),
       body: SafeArea(
         child: Center(
           child: Column(
             children: [
-              const SizedBox(height: 50,),
+              widget(
+                children: const [
+                  SizedBox(height: 50,),
+                
+                  // Below is the intended UI
+                  // Padlock logo - to represent Sign In
+                  Icon(
+                    Icons.lock,
+                    size: 100,
+                  )
+                
+                  const SizedBox(height: 50),
+                
+                  // Welcome back Sign In Text
+                  Text(
+                    'Welcome back. Sign In.',
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 16,
+                      )
+                  )
+                
+                  const SizedBox(height: 25),
+                
+                  // E-mail Text Field
+                 MyTextField,
 
-              // Below is the intended UI
-
-              // Padlock logo - to represent Sign In
-              const Icon(
-                Icons.lock,
-                size: 100,
-              )
-
-              const SizedBox(height: 50),
-
-              // Welcome back Sign In Text
-
-              // E-mail Text Field
-
-              //  Passwoed TextField
-
-              // Forgot Password?
-
-              // Sign In button
-
-              // Continue with Google or IOS option
-
-              // Don't have an account? Sign UP.
+                  //  Password TextField
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0,)
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade400),
+                      )
+                      fillColor: Colors.grey.shade200,
+                      filled: true
+                    ),
+                  )
+                  )
+                
+                  // Forgot Password?
+                
+                  // Sign In button
+                
+                  // Continue with Google or IOS option
+                
+                  // Don't have an account? Sign UP.
+                ],
+              ),
             ],
           ),
         )),
