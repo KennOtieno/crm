a4179f7942c014692d36b9ab0aa21a7a564ab52d
KennOtieno <otiken001@gmail.com>
2025-03-26 05:46:10 -0700

    Corrected some fw bugs.

--- crm/lib/components/my_button.dart
+++ crm/lib/components/my_button.dart
@@ -6,14 +6,22 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(25),
-      decoration: BoxDecoration(color: Colors.black),
-      child: Center(
+      padding: const EdgeInsets.all(25),
+      margin: EdgeInsets.symmetric(horizontal: 25),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: const Center(
         child: Text(
           "Sign In",
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
         ),
-      )
+      ),
     )
   }
 }
