9433a967be2011f23431d5c02ab9a22e87459533
KennOtieno <otiken001@gmail.com>
2025-03-28 06:40:20 -0700

    Sign Up UI 70% done.

--- crm/lib/pages/sign_up.dart
+++ crm/lib/pages/sign_up.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:crm/components/my_textfield.dart';
 
 class SignUpPage extends StatelessWidget {
-  const SignUpPage({super.key});
+  const SignUpPage({super.key}); {
+    // Initialize the Controllers 
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    confirmpasswordController = TextEditingController();
+  }
+
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+  late final TextEditingController confirmpasswordController;
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +33,31 @@ class SignUpPage extends StatelessWidget {
               const SizedBox(25),
 
               // Input E-mail Address Text Field
+              MyTextField(
+                controller: emailController,
+                hintText: 'Input a valid E-mail Address',
+                obscureText: false,
+              ),
+
+              const SizedBox(10),
 
               // Input Password Text Field
+              MyTextField(
+                controller: passwordController,
+                 hintText: 'Input your password.',
+                  obscureText: true,
+                  ),
+
+                  const SizedBox(10),
 
               // Confirm Password Text Field
+              MyTextField(
+                controller: confirmpasswordController,
+                 hintText: 'Confirm Password.',
+                  obscureText: true,
+                  )
+
+                  const SizedBox(10),
 
               // Sign Up Button
 
