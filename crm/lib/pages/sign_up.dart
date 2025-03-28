894acd24d510daf42204cd59e8d994daa94770e2
KennOtieno <otiken001@gmail.com>
2025-03-28 06:20:12 -0700

    New Updates Today

--- crm/lib/pages/sign_up.dart
+++ crm/lib/pages/sign_up.dart
@@ -5,8 +5,36 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // Sign Up design Below
-    )
+    return Scaffold (
+      backgroundColor: Colors.grey,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+
+              // Text Saying 'Welcome. Sign Up.
+              Text('Hey, Sign Up.',
+              style: TextStyle(color: Colors.grey,
+              fontSize: 16,
+              ),
+              ),
+
+              const SizedBox(25),
+
+              // Input E-mail Address Text Field
+
+              // Input Password Text Field
+
+              // Confirm Password Text Field
+
+              // Sign Up Button
+
+              // Have ann account? Sign IN.
+            ],
+          ),
+        ) 
+      )
+      ),
   }
 }
