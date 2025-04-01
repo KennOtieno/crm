d51a9023e1b0a3ab041349a1867ecdf6ac1d0db2
KennOtieno <otiken001@gmail.com>
2025-04-01 06:35:53 -0700

    Authentication File is done

--- crm/lib/pages/authentication.dart
+++ crm/lib/pages/authentication.dart
@@ -1,4 +1,6 @@
+import 'package:crm/pages/sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Authentication extends StatelessWidget {
   const Authentication({super.key});
@@ -6,7 +8,20 @@ class Authentication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-    )
+      body: StreamBuilder(
+          stream: Supabase.instance.client.auth.onAuthStateChange,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final session = snapshot.data?.session;
+
+            if (session != null) {
+              return HomePage();
+            } else {
+              return SignInPage();
+            }
+          }),
+    );
   }
 }
